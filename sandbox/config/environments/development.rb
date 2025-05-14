e499b22e01f4e9bc628a269d78a6d07b44876e89
liberaldev <ssharp0614@gmail.com>
2025-05-14 23:03:08 +0900

    config 파일 변경

--- sandbox/config/environments/development.rb
+++ sandbox/config/environments/development.rb
@@ -50,5 +50,8 @@ Sandbox::Application.configure do
   config.action_view.annotate_rendered_view_with_filenames = true
 
   # Raise error when a before_action's only/except options reference missing actions.
-  config.action_controller.raise_on_missing_callback_actions = true
+  # Rails 7.1 이상에서만 사용 가능한 옵션입니다
+  if Rails.gem_version >= Gem::Version.new('7.1.0')
+    config.action_controller.raise_on_missing_callback_actions = true
+  end
 end
